33daf51bccfbe8917ef94c9d5f58927ecc617a85
berkaysezeer <berkaysezeer@gmail.com>
2020-05-20 19:42:55 +0300

    Cors, Ui refences ,models

---
+++ MyNote.API/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyNote.API.Controllers
+{
+    public class TestController : ApiController
+    {
+        //api/test
+        [HttpGet]
+        public DateTime Time()
+        {
+            return DateTime.Now;
+        }
+    }
+}
