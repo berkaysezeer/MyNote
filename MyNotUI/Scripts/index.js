3487c9039847ea39c2975bc5b230f0311f44bc56
berkaysezeer <berkaysezeer@gmail.com>
2020-05-22 22:08:44 +0300

    login

--- MyNotUI/Scripts/index.js
+++ MyNotUI/Scripts/index.js
@@ -1,1 +1,118 @@
-﻿
+﻿var apiUrl = "https://localhost:44373/";
+
+function isLoggedIn() {
+
+}
+
+function loginData() {
+
+}
+
+function success(message) {
+    $(".tab-pane.active .message")
+        .removeClass("alert-danger")
+        .addClass("alert-success")
+        .text(message)
+        .show();
+}
+
+function error(modelState) {
+    if (modelState) {
+        var errors = [];
+        for (var prop in modelState) {
+            for (var i = 0; i < modelState[prop].length; i++) {
+                errors.push(modelState[prop][i]);
+            }
+        }
+
+        var ul = $("<ul/>");
+        for (var i = 0; i < errors.length; i++) {
+            ul.append($("<li/>").text(errors[i]));
+        }
+        $(".tab-pane.active .message")
+            .removeClass("alert-success")
+            .addClass("alert-danger")
+            .html(ul)
+            .show();
+    }
+}
+
+function errorMessage(message) {
+    if (message) {
+        $(".tab-pane.active .message")
+            .removeClass("alert-success")
+            .addClass("alert-danger")
+            .text(message)
+            .show();
+    }
+}
+
+function resetLoginForms() {
+    $(".message").hide();
+    $("#login form").each(function () {
+        this.reset();
+    });
+}
+
+$(document).ajaxStart(function () {
+    $(".loading").removeClass("d-none");
+});
+
+$(document).ajaxStop(function () {
+    $(".loading").addClass("d-none");
+});
+
+
+$("#signupform").submit(function (event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+
+    $.post(apiUrl + "api/Account/Register", formData, function (data) {
+        resetLoginForms();
+        success("Your account has been successfully created.");
+    }).fail(function (xhr) {
+        error(xhr.responseJSON.ModelState);
+    });
+
+});
+
+$("#signinform").submit(function (event) {
+    event.preventDefault();
+    var formData = $(this).serialize();
+
+    $.post(apiUrl + "Token", formData, function (data) {
+
+
+        var datastr = JSON.stringify(data); //nesneti stringe cevirir
+        if ($("#signinrememberme").prop("checked")) {
+            sessionStorage.removeItem("login")
+            localStorage["login"] = datastr;
+        } else {
+            localStorage.removeItem("login")
+            sessionStorage["login"] = datastr;
+        }
+
+        resetLoginForms();
+        success("You have been logged in successfuly. Now, you are being redirected...");
+
+
+    }).fail(function (xhr) {
+        errorMessage(xhr.responseJSON.error_description);
+    });
+
+});
+
+
+// https://getbootstrap.com/docs/4.0/components/navs/#events
+$('#login a[data-toggle="pill"]').on('shown.bs.tab', function (e) {
+    // e.target // newly activated tab
+    // e.relatedTarget // previous active tab
+
+    resetLoginForms();
+});
+
+$(".navbar-login a").click(function () {
+    var href = $(this).attr("href");
+    $('#pills-tab a[href="' + href + '"]').tab('show')
+
+});
